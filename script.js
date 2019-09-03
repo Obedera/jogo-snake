f535163e9cf70c0024ef2c1761c5816a96cf043f
Obede <obede.silva7@gmail.com>
2019-09-03 14:56:30 -0300

    gerar alimento

--- script.js
+++ script.js
@@ -7,10 +7,11 @@ for(i=0;i<256;i++){
 }
 for(i=17;i<239;i++){
     if(i%16 != 0 && (i+1)%16 != 0){
-        document.querySelectorAll('section div')[i].innerHTML = i-17;
         document.querySelectorAll('section div')[i].classList.remove('parede');
+        document.querySelectorAll('section div')[i].setAttribute('name','area');
     }
 }
+
 document.querySelectorAll('section div')[106].innerHTML = snake;
 document.querySelectorAll('section div')[106].name = snake;
 
@@ -29,10 +30,12 @@ function acharPosicao(lista){
 function checarJogo(posicao){
     if(posicao%16 == 0 || (posicao+1)%16 == 0){
         clearInterval(movimentar);
+        clearInterval(gerarAlimento);
         alert('vc perdeu');
     }
     if(posicao < 17 || posicao>238){
         clearInterval(movimentar);
+        clearInterval(gerarAlimento);
         alert('vc perdeu');
     }
 }
@@ -40,22 +43,54 @@ function checarJogo(posicao){
 function mudarPosicao(tecla){
     // Baixo
     if(tecla == 40){
-        direcao = 16;
+        if(direcao != -16){
+            direcao = 16;
+        }
     }
     // Direita
     if(tecla == 39){
-        direcao = 1;
+        if(direcao != -1){
+            direcao = 1;
+        }
     }
     // Cima
     if(tecla == 38){
-        direcao = -16;
+        if(direcao != 16){
+            direcao = -16;
+        }
     }
     // Esquerda
     if(tecla == 37){
-        direcao = -1;
+        if(direcao != 1){
+            direcao = -1;
+        }
     }    
 }
 
+function aleatorio(min, max){
+    return Math.floor(Math.random()*(max-min+1))+min;
+}
+
+function alimento(){
+    let valor;
+    let status = false;
+    let comida;
+    let area = document.querySelectorAll('section div[name="area"]');
+    for(i=0;i<area.length;i++){
+        if(area[i].innerHTML == '@'){
+            comida = i;
+        }
+    }
+    if(comida == null){
+        while (status==false){
+            valor = aleatorio(0,195);
+            if(area[valor].innerHTML == ''){
+                area[valor].innerHTML = '@';
+                status=true
+            }
+        }
+    }
+}
 
 let movimentar = setInterval(function(){
     let campo = document.querySelectorAll('section div');
@@ -66,5 +101,9 @@ let movimentar = setInterval(function(){
     campo[posicao+direcao].name = snake;
     campo[posicao+direcao].innerHTML = snake;
 },300);
+
+let gerarAlimento = setInterval(function(){
+    alimento()
+},5000)
 
 document.addEventListener('keydown', pegarTecla)

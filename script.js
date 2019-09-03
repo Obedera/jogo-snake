cbd19f4bbd3f0dd90c96b6af31821536b57e579f
Obede <obede.silva7@gmail.com>
2019-09-03 13:31:18 -0300

    definir limite

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 let jogo = document.querySelector('section');
 let snake = '$';
+let direcao = 1;
 
-for(i=0;i<225;i++){
-    jogo.innerHTML += '<div name="">'+i+'</div>';
+for(i=0;i<256;i++){
+    jogo.innerHTML += '<div class="parede" name=""></div>';
 }
-document.querySelectorAll('section div')[105].innerHTML = snake;
-document.querySelectorAll('section div')[105].name = snake;
+for(i=17;i<239;i++){
+    if(i%16 != 0 && (i+1)%16 != 0){
+        document.querySelectorAll('section div')[i].innerHTML = i-17;
+        document.querySelectorAll('section div')[i].classList.remove('parede');
+    }
+}
+document.querySelectorAll('section div')[106].innerHTML = snake;
+document.querySelectorAll('section div')[106].name = snake;
 
 function pegarTecla(event){
     return mudarPosicao(event.keyCode);
@@ -19,21 +26,21 @@ function acharPosicao(lista){
     }
 }
 
-let direcao = 1;
-let movimentar = setInterval(function(){
-        let campo = document.querySelectorAll('section div');
-        let posicao = acharPosicao(campo);
-        campo[posicao].name = '';
-        campo[posicao].innerHTML = '';
-        campo[posicao+direcao].name = snake;
-        campo[posicao+direcao].innerHTML = snake;
-},300);
-
+function checarJogo(posicao){
+    if(posicao%16 == 0 || (posicao+1)%16 == 0){
+        clearInterval(movimentar);
+        alert('vc perdeu');
+    }
+    if(posicao < 17 || posicao>238){
+        clearInterval(movimentar);
+        alert('vc perdeu');
+    }
+}
 
 function mudarPosicao(tecla){
     // Baixo
     if(tecla == 40){
-        direcao = 15;
+        direcao = 16;
     }
     // Direita
     if(tecla == 39){
@@ -41,7 +48,7 @@ function mudarPosicao(tecla){
     }
     // Cima
     if(tecla == 38){
-        direcao = -15;
+        direcao = -16;
     }
     // Esquerda
     if(tecla == 37){
@@ -49,5 +56,15 @@ function mudarPosicao(tecla){
     }    
 }
 
+
+let movimentar = setInterval(function(){
+    let campo = document.querySelectorAll('section div');
+    let posicao = acharPosicao(campo);
+    campo[posicao].name = '';
+    campo[posicao].innerHTML = '';
+    checarJogo(posicao+direcao);
+    campo[posicao+direcao].name = snake;
+    campo[posicao+direcao].innerHTML = snake;
+},300);
 
 document.addEventListener('keydown', pegarTecla)
